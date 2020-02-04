0766fc6891d1901ef47ce8fe4e88ee6d09bd6e51
Brian de Paula Mello <35143307+Brian-Mello@users.noreply.github.com>
2020-02-04 16:34:59 -0300

    card estático terminado

--- src/containers/CartPage/index.js
+++ src/containers/CartPage/index.js
@@ -1,23 +1,44 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { CardContainer, CardAdressSection, CardMain } from './styled';
-import { StyledButtonForms } from "../../style/styled";
+import { CardContainer, CardAdressSection, CardMain, ShippingContainer, StyledH4, SubTitle, TotalContainer, StyledButtonConfirm } from './styled';
 import Footer from '../Footer';
 import Header from '../../components/Header';
+import { push } from 'connected-react-router';
+import { routes } from '../Router';
+import CustomizedRadios from '../../components/PaymentMethod';
 
 export class CartPage extends Component {
+    constructor(props){
+        super(props);
+
+        this.state={
+
+        }
+    }
     render() {
         const { goToFeedPage } = this.props
         return (
             <CardContainer>
                 <Header title="Meu Carrinho"/>
                 <CardAdressSection>
-
+                    <p>Endereço de entrega</p>
                 </CardAdressSection>
                 <CardMain>
                     <p>Carrinho Vazio</p>
                 </CardMain>
-                <StyledButtonForms onClick={goToFeedPage}>Entrar</StyledButtonForms>
+                <ShippingContainer>
+                    <p>Frete R$0,00</p>
+                </ShippingContainer>
+                <TotalContainer>
+                    <h3>SUBTOTAL</h3>
+                    <p></p>
+                    <h3>R$00,00</h3>
+                </TotalContainer>
+                <SubTitle>
+                    <StyledH4>Forma de Pagamento</StyledH4>
+                </SubTitle>
+                <CustomizedRadios/>
+                <StyledButtonConfirm>Confirmar</StyledButtonConfirm>
                 <Footer/>
             </CardContainer>
         )
@@ -29,7 +50,7 @@ const mapStateToProps = (state) => ({
 })
 
 const mapDispatchToProps = dispatch => ({
-
+    goToFeedPage: () => dispatch(push(routes.feedPage))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(CartPage)
